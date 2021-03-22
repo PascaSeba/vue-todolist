fb5311b98a566075a0977ed1fb0ff427201cff52
Sebastiano <pasca.sebastiano5@libero.it>
2021-03-22 16:57:30 +0100

    Esercizio in corso

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
+/* 
+Ricreare l'esercizio della to do list come fatto assieme
+in classe:
+- stampare in pagina un item per ogni elemento contenuto in un array
+- ogni item ha una "x" associata: cliccando su di essa, 
+  l'item viene rimosso dalla lista
+- predisporre un input per aggiungere un nuovo item alla lista: 
+  digitando il tasto invio oppure ciccando su un pulsante, 
+  il testo digitato viene aggiunto alla lista 
+  */
+
 var app = new Vue({
     el: '#root',
     data: {
+        myToDo: (""),
         toDoList: [
-            "Fare attività fisica", "Salutare il tizio urlante dei limoni", "Fare una spremuta i limoni"
+            "Fare attività fisica", "Salutare il tizio urlante dei limoni", "Fare una spremuta di umani"
         ],
-        inputext: "ciao",
     },
-})
+    methods: {
+        addToDo() {
+            this.toDoList.push(this.myToDo);
+            this.myToDo = ("");
+        },
+        deleteToDo(index) {
+            this.toDoList.splice(index, 1);
+            alert("Complimenti! Hai svolto un TO-DO!");
+        }
+    }
+});
 Vue.config.devtools = true;
